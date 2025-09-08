3ea96785a0793f32142ddf772f0e79fd001aa280
Dileepsai1083 <dileepsai1083@gmail.com>
2025-09-08 22:29:31 +0530

    Add files via upload

---
+++ 1,Python Program to check if a Number Is Positive Or Negative.py
@@ -0,0 +1,7 @@
+number=int(input("enter the number"))
+if number<0:
+    print(f'{number} is a negitive number')
+elif number==0:
+    print('zero')
+else:
+    print(f'{number} is a positive number')
