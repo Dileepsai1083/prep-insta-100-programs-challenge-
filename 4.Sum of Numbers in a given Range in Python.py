7543c33b31eb2e78aeb7ca2d07a3bb865caf32a2
Dileepsai1083 <dileepsai1083@gmail.com>
2025-09-08 22:54:10 +0530

    Add files via upload

---
+++ 4.Sum of Numbers in a given Range in Python.py
@@ -0,0 +1,7 @@
+number1=int(input("enter the 1st number"))
+number2=int(input("enter the 2nd number"))
+sum=0
+for i in range(number1,number2+1):
+    sum+=i
+print(f'sum between {number1}&{number2} is {sum}')
+    
